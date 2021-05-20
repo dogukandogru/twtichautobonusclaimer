460194bae12aaf16bd021fd33d0e84879eb7b74e
Dogukan <dogukandogruu@gmail.com>
2021-05-20 14:52:17 +0300

    class name changed

--- claimer.js
+++ claimer.js
@@ -1,5 +1,5 @@
 function claimBonus(){
-    var buttons = document.getElementsByClassName("tw-button tw-button--success")
+    var buttons = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 VGQNd")
     for(var i = 0; i<buttons.length; i++){
         buttons[i].click();
     }

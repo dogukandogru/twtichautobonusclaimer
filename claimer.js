b6d5dae6e2b30d8f928741f61d1459b8fa700816
Doğukan Doğru <48735043+dogukandogru@users.noreply.github.com>
2021-08-13 13:15:21 +0300

    class name updated

--- claimer.js
+++ claimer.js
@@ -1,5 +1,5 @@
 function claimBonus(){
-    var buttons = document.getElementsByClassName("sc-fzozJi sc-fznKkj jwRWhW")
+    var buttons = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 fERWGf")
     for(var i = 0; i<buttons.length; i++){
         buttons[i].click();
     }
